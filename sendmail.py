743866755140e5482866745492140c104a475aaf
Milos Vasic <milos85vasic@gmail.com>
2020-06-19 10:57:24 +0200

    Port 465.

--- sendmail.py
+++ sendmail.py
@@ -1,17 +1,17 @@
 import ssl
 import smtplib
 
-port = 587
+port = 465
 smtp_server = "test1.dev.local"
 sender_email = "test1@dev.local"
-receiver_email = "test1@dev.local"
+receiver_email = "test2@dev.local"
 password = "Test12345"
 message = "Test message"
 
 context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()
-    server.starttls(context=context)
+with smtplib.SMTP_SSL(host=smtp_server, port=port, context=context) as server:
     server.ehlo()
     server.login(sender_email, password)
+    server.ehlo()
     server.sendmail(sender_email, receiver_email, message)
+    server.quit()
